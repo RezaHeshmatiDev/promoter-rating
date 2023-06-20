16a41288a87247d4bb5ad63b84e1b175933822bd
rezaheshmatidev <rezaheshmatidev@gmail.com>
2023-06-20 14:52:49 +0330

    fix style bug in 404 page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,20 +1,22 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 const PageNotFound = () => {
   const navigate = useNavigate();
 
   return (
-    <Box>
-      <Typography>{"404"}</Typography>
-      <Typography>{"چنین صفحه ای وجود ندارد"}</Typography>
-      <Button
-        variant={"contained"}
-        onClick={() => navigate("/", { replace: true })}
-      >
-        <Typography>{"بازگشت به صفحه اصلی"}</Typography>
-      </Button>
-    </Box>
+    <Container sx={{ width: "100%", height: "100%", display: "flex", justifyContent: "center" }}>
+      <Box >
+        <Typography>{"404"}</Typography>
+        <Typography>{"چنین صفحه ای وجود ندارد"}</Typography>
+        <Button
+          variant={"contained"}
+          onClick={() => navigate("/", { replace: true })}
+        >
+          <Typography>{"بازگشت به صفحه اصلی"}</Typography>
+        </Button>
+      </Box>
+    </Container>
   );
 };
 
