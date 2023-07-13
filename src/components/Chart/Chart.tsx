6a23374226928ffd1f0f412ca4b23b8606e5d169
Amir Sorayaei <amir.s1377@gmail.com>
2023-07-13 10:44:38 +0330

    Minor changes on fonts and names on the chart

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,4 +1,4 @@
-import "chart.js/auto";
+import { defaults } from "chart.js/auto";
 import { Box } from "@mui/material";
 import { Chart as ReactChart } from "react-chartjs-2";
 import { InvoiceData, InvoiceDataProps } from "../../pages/Dashboard";
@@ -10,6 +10,9 @@ interface Props {
 const Chart = ({ data }: Props) => {
   const datasets: InvoiceDataProps[] = data ? Object.values(data) : [];
 
+  defaults.font.family = "SNM";
+  defaults.font.weight = "bold";
+
   return (
     <Box mb={3}>
       <ReactChart
@@ -18,15 +21,15 @@ const Chart = ({ data }: Props) => {
           labels: ["امروز", "دیروز", "دو روز قبل", "سه روز قبل"],
           datasets: [
             {
-              label: "allInvoicesCount",
+              label: "کل فاکتورها",
               data: datasets.map((item) => item.allInvoicesCount),
             },
             {
-              label: "ratedInvoicesCount",
+              label: "نظر داده شده",
               data: datasets.map((item) => item.ratedInvoicesCount),
             },
             {
-              label: "unratedInvoices",
+              label: "نظر داده نشده",
               data: datasets.map((item) => item.unratedInvoices),
             },
           ],
